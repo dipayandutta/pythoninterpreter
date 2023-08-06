1e8646b66093908892d976b43a392fbdfeeb8af0
Dipayan Dutta <inbox.dipayan@gmail.com>
2023-08-06 14:56:38 +0530

    main file for the program

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+from lexer import Lexer
+from parser_ import Parser
+
 while True:
     text = input('calc>')
+    lexer =  Lexer(text)
+    tokens = lexer.generate_tokens()
+    
+    parser = Parser(tokens)
+    tree = parser.parse()
+    print(tree)
